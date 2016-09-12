9414233e3da4ea064f9c6e4f91c1e8d6465a709c
dkoslicki <dmkoslicki@gmail.com>
2016-09-12 17:35:23 -0500

    typo fix

--- src/Julia/IRONNLSQ.jl
+++ src/Julia/IRONNLSQ.jl
@@ -7,13 +7,13 @@
 # ==============================================================================
 include("lsqnonneg.jl")
 
-function IRONNLSQ(Aaux,s)
+function IRONNLSQ(Aaux,s,q,nIter,myeps,I)
 	q = 1/100;  # diversity index
 	nIter = 3;  # Number of iterations to do for
 	myeps = 0.00001;  # reweighting epsilon?!
-	N = size(A,2);  # Number of columns
 	I = eye(N);
 	A = Aaux[setdiff(1:end, 1), :];  # delete first row of 1's
+	N = size(A,2);  # Number of columns
 	weightI = ones(1,N);  # Z=I
 	for n=1:nIter
 		Atilde = [weightI; lambda*A];  # Stick the weights back in
