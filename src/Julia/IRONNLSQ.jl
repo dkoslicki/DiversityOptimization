d3b0c123a112c9bb5db312fbea079284ef9b589c
dkoslicki <dmkoslicki@gmail.com>
2016-09-13 10:23:00 -0500

    back to original

--- src/Julia/IRONNLSQ.jl
+++ src/Julia/IRONNLSQ.jl
@@ -13,17 +13,18 @@ function IRONNLSQ(Aaux,s,q,nIter,myeps,I)
 #	myeps = 0.00001;  # reweighting epsilon?!
 #	I = eye(N);
 
-	x = lsqnonneg(Aaux,s);
-	return x
+	#x = lsqnonneg(Aaux,s);
+	#return x
 
 # Removed for debug
 #	A = Aaux[setdiff(1:end, 1), :];  # delete first row of 1's
-#	N = size(A,2);  # Number of columns
-#	weightI = ones(1,N);  # initial weights are 1
-#	for n=1:nIter
-#		Atilde = [weightI; lambda*A];  # Stick the weights back in
-#		xI = lsqnonneg(Atilde,s);
-#		weightI = (I*xI+myeps)'.^(q-1);  # Do the reweighting
-#	end
-#	return xI
+	N = size(A,2);  # Number of columns
+	weightI = ones(1,N);  # initial weights are 1
+	for n=1:nIter
+		#Atilde = [weightI; lambda*A];  # Stick the weights back in
+		Aaux[1,:] = weightI;
+		xI = lsqnonneg(Atilde,s);
+		weightI = (I*xI+myeps)'.^(q-1);  # Do the reweighting
+	end
+	return xI
 end
